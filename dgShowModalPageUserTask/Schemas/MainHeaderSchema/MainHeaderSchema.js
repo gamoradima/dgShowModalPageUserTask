cab2a0176de677f1d6b831b171843d5b2acd3789
Dmitriy Gamora <gamoradima@gmail.com>
2022-10-25 12:14:44 +0300

    Localization for en-US added, main header label restored

--- dgShowModalPageUserTask/Schemas/MainHeaderSchema/MainHeaderSchema.js
+++ dgShowModalPageUserTask/Schemas/MainHeaderSchema/MainHeaderSchema.js
@@ -4,14 +4,14 @@ define("MainHeaderSchema", ["dgShowModalPageUserTaskMixin"], function() {
         	ShowModalPageUserTaskMixin: "Terrasoft.ShowModalPageUserTaskMixin"
         },
 		methods: {
-			_initConfigurationVersion: function() {
+			/*_initConfigurationVersion: function() {
 				let userName = Terrasoft.SysValue.CURRENT_USER.displayValue;
 				let version = userName + " " + Terrasoft.productVersion;
 				if (Terrasoft.isDebug) {
 					version += " Debug";
 				}
 				this.set("ConfigurationVersion", version);
-			},
+			},*/
 			init: function() {
 				this.callParent(arguments);
 				this.dgSubscribeForShowModalWindowServerChannelMessages();
